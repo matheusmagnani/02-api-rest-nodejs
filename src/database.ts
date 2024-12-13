75af6db66b3955bc3ee8b9c1fc68aabc76665568
Matheus Magnani <mrmatheusmagnani@gmail.com>
2024-12-13 17:02:57 -0300

    Initial app and error of DATABASE_URL

---
+++ src/database.ts
@@ -0,0 +1,23 @@
+import { knex as setupKnex, Knex } from 'knex';
+import { env } from './env';
+
+console.log(process.env)
+
+if(!process.env.DATABASE_URL){
+  throw new Error('DATABASE_URL env not found')
+}
+
+export const config: Knex.Config = {
+  client: 'sqlite3', 
+  connection: {
+    filename: env.DATABASE_URL
+  },
+
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations"
+  }
+}
+
+export const knex = setupKnex(config)
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+import { transactionsRoutes } from './routes/transactions'
+import { env } from "./env";
+
+
+const app = fastify();
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions'
+})
+
+app.listen({
+  port: env.PORT
+}).then(() => {
+  console.log("HTTP server Running")
+})
