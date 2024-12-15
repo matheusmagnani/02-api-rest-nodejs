3217a59a661371233837441a95a384cb44328be5
matheusmagnani <mrmatheusmagnani@gmail.com>
2024-12-15 20:55:37 -0300

    creation and listing of transactions, and validation of cookies

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from "fastify";
 import { transactionsRoutes } from './routes/transactions'
 import { env } from "./env";
+import  cookie from "@fastify/cookie";
 
 
 const app = fastify();
+
+app.register(cookie)
 
 app.register(transactionsRoutes, {
   prefix: 'transactions'
